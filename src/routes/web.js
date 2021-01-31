ce6b77217040267316d3f513304ac7926d902ffe
Leonaye10 <ngonkouralionel@gmail.com>
2021-01-31 20:10:08 +0100

    deploy heroku

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import chatBotController from '../controllers/chatBotController'
 import homepageController from '../controllers/homepageController'
 
 let router = express.Router()
 
 let initWeRoutes = (app) => {
     router.get("/", homepageController.getHomepage)
+    router.get("/webhook", chatBotController.getWebHook)
+    router.post("/wrbhook", chatBotController.postWebHook)
 
     return app.use("/", router);
 }
